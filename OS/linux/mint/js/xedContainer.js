3efca174488864e9352c9f5e48e4b1f6ef847e76
lacapsule <info@lacapsule.org>
2024-04-07 11:54:49 +0200

    MAJ xed mint

---
+++ OS/linux/mint/js/xedContainer.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionne tous les liens avec target="windowElement"
+    const links = document.querySelectorAll('a[target="windowElement"]');
+
+    function handleOpenwindow(link) {
+        const container = document.querySelector(`section[data-link="${link.dataset.link}"]`);
+        
+        if (container) {
+            if (container.style.display === "none") {
+                container.style.display = "block";
+            
+                link.classList.add('active-link');
+                // Ajouter la classe active à l'élément de fenêtre
+                container.classList.add('active');
+            } else {
+                container.style.display = "none";
+                // Retirer la classe active de l'élément de fenêtre
+                container.classList.remove('active');
+                container.classList.remove('windowElementActive');
+            }
+        }
+    }
+
+    // Lancer la fonction d'ouverture pour chaque lien
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Empêche le comportement par défaut du lien
+            handleOpenwindow(this); // Utilisez 'this' pour référencer l'élément de lien
+        });
+    });
+});
